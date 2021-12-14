14392f26441fb9c6f811719f893e1870e6b0a3b3
gibbsnich <gibbsnich@gmail.com>
2021-12-14 13:06:31 +0100

    fix recipe id and replacing

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -51,7 +51,7 @@ const store = createStore({
         },
         storeRecipe(state, recipe) {
             if (!recipe.id) {
-                const nextId = state.recipes.reduce((r, s) => r.id > s ? r.id : s, 0) + 1;
+                const nextId = state.recipes.reduce((s, r) => r.id > s ? r.id : s, 0) + 1;
                 recipe.id = nextId;
             }
             state.recipes.push(recipe);
@@ -60,10 +60,8 @@ const store = createStore({
             const recipe = state.recipes.filter((r) => r.id === event.recipeId)[0];
             if (!recipe || recipe.length === 0)
                 return;
-            
-            if (event.extendedProps.recur) {
-                let eventDate = dateToString(event.start),
-                    eventStart, eventEnd;
+            let eventDate = dateToString(event.start), eventStart, eventEnd;
+            if (event.extendedProps.recur) {    
                 if (event.title === "Mittagessen") {
                     eventStart = `${eventDate}T12:00`;
                     eventEnd = `${eventDate}T13:00`;
@@ -71,13 +69,12 @@ const store = createStore({
                     eventStart = `${eventDate}T18:00`;
                     eventEnd = `${eventDate}T19:00`;
                 }
+            }
+            const startISO = eventStart ? eventStart : dateToTimeString(event.start);
+            const selEvent = state.events.filter((evt) => evt.start === startISO);
+            if (selEvent.length === 0) {
                 state.events.push({title: recipe.name, start: eventStart, end: eventEnd, color: 'red', extendedProps: {recipeId: recipe.id}});
             } else {
-                const startISO = dateToTimeString(event.start);
-                const selEvent = state.events.filter((evt) => evt.start === startISO);
-                if (selEvent.length === 0) {
-                    return;
-                }
                 selEvent[0].title = recipe.name;
                 selEvent[0].extendedProps.recipeId = recipe.id;
             }
